e51db8a720fed3c36a958722c17326c7bd4e7793
KUSH PATEL <kushp184@gmail.com>
2024-10-25 14:14:03 +0530

    Update Q3.c

--- project 4/Q3.c
+++ project 4/Q3.c
@@ -1,6 +1,17 @@
-#include<stdio.h>
-
-int main()
+#include <stdio.h>
+void main()
 {
-    
+    for (int i = 1; i <= 5; i++)
+    {
+
+        for (int k = i; k <= 4; k++)
+        {
+            printf(" ");
+        }
+        for (int j = 6 - i; j <= 5; j++)
+        {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
 }
